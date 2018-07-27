dce0df404b1d18d01052c90bd20bba6f81e1e0c4
baamosk <bunth667@gmail.com>
2018-07-27 00:36:02 -0400

    my first c-sharp project

---
+++ test/test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace test
+{
+    class printTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine('Hello World');
+        }
+    }
+}
